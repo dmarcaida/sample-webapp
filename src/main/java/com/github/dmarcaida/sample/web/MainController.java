22fde1bdf93449c32cb699d08d222dd743d8e12d
DJ Marcaida <marcaidadj@yahoo.com>
2016-10-06 22:30:58 +0800

    Initial commit

---
+++ src/main/java/com/github/dmarcaida/sample/web/MainController.java
@@ -0,0 +1,21 @@
+package com.github.dmarcaida.sample.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class MainController {
+
+    @RequestMapping("/home")
+    ResponseEntity<String> home() {
+        return ResponseEntity.ok("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainController.class);
+    }
+}
