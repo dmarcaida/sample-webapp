2bb2382ec6b5f3d8ac13e8611211340f075f690b
DJ Marcaida <marcaidadj@yahoo.com>
2016-10-08 19:20:49 +0800

    shoot the moon

--- src/main/java/com/github/dmarcaida/sample/web/MainController.java
+++ src/main/java/com/github/dmarcaida/sample/web/MainController.java
@@ -1,13 +1,13 @@
 package com.github.dmarcaida.sample.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
-@EnableAutoConfiguration
 public class MainController {
 
     @RequestMapping
